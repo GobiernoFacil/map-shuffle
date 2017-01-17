82013ae5526ef5ccaf4a971cee0be38485e6ad1a
elcoruco <el.coruco@gmail.com>
2017-01-17 16:30:55 -0600

    add base card template

--- card.js
+++ card.js
@@ -1,9 +1,16 @@
-var DatosGobMxURL = "http://api.datos.gob.mx/v1/proyectos-opa?" + encodeURIComponent("cve-ppi") + "='08096480005";
+var DatosGobMxURL = "http://localhost:8000/api/test";
+//"http://api.datos.gob.mx/v1/proyectos-opa?" + encodeURIComponent("cve-ppi") + "='08096480005";
 
 
 console.log(window.location.hash);
+
+
 d3.json(DatosGobMxURL)
-    //.mimeType("text/csv")
-    .get({"cve-ppi" : "'08096480005"}, function(e, d){
-      console.log(e, d);
-    });
+  .get(function(e, d){
+    var GFapp = new Vue({
+      el   : '#GF-card',
+      data : d
+    })
+  });
+    
+
