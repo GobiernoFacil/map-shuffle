1601ba9123223b804f211103ad7dae9ae18074ed
elcoruco <el.coruco@gmail.com>
2017-06-15 13:32:32 -0500

    add circle render

--- js/card.helpers.js
+++ js/card.helpers.js
@@ -29,7 +29,6 @@
       },
 
       render : function(data, container, template){
-        console.log(this);
         data.forEach(function(d){
           var div = document.createElement("div"),
               vue;
@@ -37,17 +36,16 @@
           div.innerHTML = template;
           container.appendChild(div);
 
+          this.renderCircle(div, d, this.config.circleClass);
+          
           vue = new Vue({
             el   : div,
             data : d
           });
 
-          this.renderCircle(div, d, this.config.circleClass);
           // function(el, data, className){
 
 
-
-          //console.log(this, this.vueTemplates);
 
           //this.vueTemplates.push(vue);
 
@@ -133,7 +131,68 @@ d3.json(endpoint)
       }
 
       divs = Array.prototype.slice.apply(divs);
-      //console.log(divs);
+
+      divs.forEach(function(div){
+        var width  = div.getAttribute("data-width"),
+            height = div.getAttribute("data-height"),
+            colorA = div.getAttribute("data-color-a"),
+            colorB = div.getAttribute("data-color-b"),
+            column = div.getAttribute("data-column"),
+            radius = Math.min(width, height) / 2,
+            value  = +data[column],
+            color, arc, pie, svg, g, _data;
+
+
+        color = d3.scale.ordinal()
+                  .range([colorA, colorB]);
+
+        arc = d3.svg.arc()
+                    .outerRadius(function(d,i) {
+                      return i ? radius - 20 : radius - 10;
+                    })
+                    .innerRadius(function(d,i) {
+                      return i ? radius - 15 : radius - 25;
+                    });
+
+        pie = d3.layout.pie()
+                .sort(null)
+                .value(function(d) { 
+                  return d.amount; 
+                });
+
+
+        console.log(div);
+
+        svg = d3.select(div).append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+        _data = [ {"amount": value }, {"amount": 100 - value} ];
+
+        g = svg.selectAll(".arc")
+          .data(pie(_data))
+        .enter().append("g")
+          .attr("class", "arc");
+
+      g.append("path")
+          .attr("d", arc)
+          .style("fill", function(d) { 
+            return color(d.data.amount); });
+
+      g.append("text")
+          .attr("transform",  "translate(-25)")
+          .attr("dy", ".35em")
+          .text(function(d, i) {
+            if(i==0) {
+             return d.data.amount + "%";
+            }
+          })
+        .attr("class", "text_arc");
+
+      }, this);
     }
     /*
     GFMakeCircle(settings){
