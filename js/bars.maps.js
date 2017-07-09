179c266e4499854666dab63fefcae52c250d0e14
Boris Cuapio Morales <el.coruco@gmail.com>
2017-07-08 23:06:33 -0500

    add base functions for comparation

--- js/bars.maps.js
+++ js/bars.maps.js
@@ -11,20 +11,55 @@ define(function(require){
   // [1] obtiene los templates de UI
   // ----------------------------------------------------------------------
   // 
-  var TEMPLATE = require("text!templates/bars-app.html");
+  var TEMPLATE = require("text!templates/bars-app.html"),
+      FILTER   = require("text!templates/filter-item.html");
 
   // [2] define el constructor
   // ----------------------------------------------------------------------
   //
   var GraphsControllerConstructor = function(parent){
 
-    var _id        = parent.settings.ui.barsTool.container,
-        _container = document.getElementById(_id),
-        _values    = null,
-        _states    = parent.lists.estadosName,
-        _cities    = parent.lists.municipiosName,
-        _branches  = parent.lists.ramosName,
-        _units     = parent.lists.unidadesName;
+/*
+      "barsTool" : {
+      "container"         : "GF-SHCP-bars-tool",
+      "singleLocation"    : "GF-SHCP-bars-app-single-location",
+      "multipleLocations" : "GF-SHCP-bars-app-multiple-locations",
+      "singleLocationUI" : {
+        "state"           : "GF-SHCP-bars-app-single-location-state",
+        "city"            : "GF-SHCP-bars-app-single-location-city",
+        "addLocation"     : "GF-SHCP-bars-app-single-location-select-btn",
+        "selectedFilters" : "GF-SHCP-bars-app-single-location-selected-filters",
+        "filterList"      : "GF-SHCP-bars-app-single-location-filter-list",
+        "filterPrefix"    : "GF-SHCP-bars-app-single-location-filter-prefix-",
+        "addGraph"        : "GF-SHCP-bars-app-single-location-add-bar-btn"   
+      }
+    },
+      */
+
+    var _id         = parent.settings.ui.barsTool.container,
+        _slID       = parent.settings.ui.barsTool.singleLocation,
+        _mlID       = parent.settings.ui.barsTool.multipleLocations,
+        _container  = document.getElementById(_id),
+        _sContainer = null, 
+        _mContainer = null,
+        _values     = null,
+        _data       = null,
+        _states     = parent.lists.estadosName.states,
+        _cities     = parent.lists.municipiosName,
+        _branches   = parent.lists.ramosName,
+        _units      = parent.lists.unidadesName,
+
+        _SLStateInputID      = parent.settings.ui.barsTool.singleLocationUI.state,
+        _SLCityInputID       = parent.settings.ui.barsTool.singleLocationUI.city,
+        _SLLocationBtnID     = parent.settings.ui.barsTool.singleLocationUI.addLocation,
+        _SLSelectedFiltersID = parent.settings.ui.barsTool.singleLocationUI.selectedFilters,
+        _SLFilterListID      = parent.settings.ui.barsTool.singleLocationUI.filterList,
+        _SLFilterPrefix      = parent.settings.ui.barsTool.singleLocationUI.filterPrefix,
+        _SLAddGraphBtn       = parent.settings.ui.barsTool.singleLocationUI.addGraph,
+
+
+        SELECTALL = "ALL";
+
 
   // [3] define el objeto del comparador
   // ----------------------------------------------------------------------
@@ -33,19 +68,17 @@ define(function(require){
 
       render : function(){
         _values = parent.currentMap.config.values;
+        _data   = parent.currentMap.data; 
 
         if(_values.length){
           this.show();
           this.renderSingleLocation();
           this.renderMultipleLocation();
-          //this.renderOptions();
           return;
         }
         else{
           this.hide();
         }
-
-
       },
 
       show : function(){
@@ -58,36 +91,63 @@ define(function(require){
         _container.style.display = "none";
       },
 
-      renderOptions : function(){
-        var filtersA = parent.currentMap.config.filters,
-            filtersB = parent.currentMap.config.extraFilters;
-
-        renderValueList();
-
-        if(this._findFilter("state", filtersA)){
-
-        }
-      },
-
-      
+      /*
+      _SLStateInputID      = parent.settings.ui.barsTool.singleLocationUI.state,
+        _SLCityInputID       = parent.settings.ui.barsTool.singleLocationUI.city,
+        _SLLocationBtnID     = parent.settings.ui.barsTool.singleLocationUI.addLocation,
+        _SLSelectedFiltersID = parent.settings.ui.barsTool.singleLocationUI.selectedFilters,
+        _SLFilterListID      = parent.settings.ui.barsTool.singleLocationUI.filterList,
+        _SLFilterPrefix      = parent.settings.ui.barsTool.singleLocationUI.filterPrefix,
+        _SLAddGraphBtn       = parent.settings.ui.barsTool.singleLocationUI.addGraph;
+      */
 
       renderSingleLocation : function(){
+        var container = document.getElementById(_slID),
+            state     = document.getElementById(_SLStateInputID),
+            city      = document.getElementById(_SLCityInputID),
+            filters   = document.getElementById(_SLFilterListID);
 
+        this.renderStateList(state);
+        this.renderCityList(city);
+        this.renderOptions(filters);
+
+        //console.log(container);
+
+        container.addEventListener("submit", function(e){e.preventDefault()});
       },
 
       renderMultipleLocation : function(){
 
       },
 
+      renderOptions : function(container){
+        var filtersA = parent.currentMap.config.filters,
+            filtersB = parent.currentMap.config.extraFilters;
+
+        this.renderValueList(container);
+      },
+
       renderValueList : function(){
 
       },
 
-      renderStateList : function(){
+      renderStateList : function(state){
+        console.log(_data);
+        console.log(state);
+        console.log(_states);
 
+        /*
+        32
+name
+:
+"Zacatecas"
+url
+:
+"zacatecas"
+        */
       },
 
-      renderCityList : function(){
+      renderCityList : function(city){
 
       },
 
