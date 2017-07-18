99db447bf540b98d285d2f5f7bd2d1904f27c2e0
elcoruco <el.coruco@gmail.com>
2017-07-18 13:34:16 -0500

    enable data download

--- js/search.maps.js
+++ js/search.maps.js
@@ -7,9 +7,10 @@
 
 define(function(require){
 
-  var d3       = require("d3"),
-      TEMPLATE = require("text!templates/advanced-search.html"),
-      CONFIG   = require("json!config/config.map.json");
+  var d3         = require("d3"),
+      underscore = require("underscore"),
+      TEMPLATE   = require("text!templates/advanced-search.html"),
+      CONFIG     = require("json!config/config.map.json");
 
   var SearchControllerConstructor = function(parent){
 
@@ -26,6 +27,7 @@ define(function(require){
         table      = null,
         tbody      = null,
         thead      = null,
+        download   = null,
         page       = 0;
 
     var controller = {
@@ -37,13 +39,17 @@ define(function(require){
         data       = currentMap.data.slice();
         DATA       = currentMap.data.slice();
         container.innerHTML = TEMPLATE;
-        table = container.querySelector("table");
-        thead = table.querySelector("thead");
-        tbody = table.querySelector("tbody");
+        table     = container.querySelector("table");
+        thead     = table.querySelector("thead");
+        tbody     = table.querySelector("tbody");
+        download  = document.getElementById(UI.downloadBtn);
+
+        console.log(UI.downloadBtn);
 
         this.renderHeaders();
         this.renderItems(page);
         this.renderPagination(page);
+        this.enableDowload();
       },
 
       renderHeaders : function(){
@@ -87,6 +93,46 @@ define(function(require){
 
         currentPage.value = page + 1;
         totalPages.innerHTML = pages;
+      },
+
+      enableDowload : function(){
+        // implementado de:
+        // https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side/24922761#24922761
+        download.addEventListener("click", function(e){
+          e.preventDefault();
+
+          var firstItem  = data[0],
+              keys       = data[0] ? _.keys(data[0]) : null,
+              _data      = [],
+              csvContent = "data:text/csv;charset=utf-8,",
+              response,
+              encodedUri;
+
+          if(!keys) return;
+
+          data.forEach(function(d){
+            var arr = [];
+            keys.forEach(function(key){
+              arr.push(d[key]);
+            });
+
+            _data.push(arr);
+          });
+
+          _data.unshift(keys);
+
+          _data.forEach(function(infoArray, index){
+            infoArray = infoArray.map(function(str){
+              return "\"" + _.escape(str) + "\"";
+            });
+            dataString = infoArray.join(",");
+            csvContent += index < data.length ? dataString+ "\n" : dataString;
+          });
+
+          encodedUri = encodeURI(csvContent);
+          window.open(encodedUri);
+
+        });
       }
 
       
